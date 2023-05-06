525eaaff5a4b018bbfb345115b5cd1cb89cd3042
Abraham Tesfay <abraham@Abrahams-Mac-mini.local>
2023-05-06 01:42:33 -0700

    Add Toaster and update the RegisterModal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,32 @@
-import ClientOnly from './components/ClientOnly'
-import RegisterModal from './components/modals/RegisterModal'
-import Navbar from './components/navbar/Navbar'
-import './globals.css'
-import { Nunito } from 'next/font/google'
+import ClientOnly from "./components/ClientOnly";
+import RegisterModal from "./components/modals/RegisterModal";
+import Navbar from "./components/navbar/Navbar";
+import "./globals.css";
+import { Nunito } from "next/font/google";
+import ToasterProvider from "./providers/ToasterProvider";
 
-const font = Nunito({ subsets: ['latin'] })
+const font = Nunito({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb colne web app',
-}
+  title: "Airbnb",
+  description: "Airbnb colne web app",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <ToasterProvider />
           <RegisterModal />
           <Navbar />
         </ClientOnly>
         {children}
       </body>
     </html>
-  )
+  );
 }
