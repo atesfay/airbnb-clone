cf712809f384306977f9410084be364e1d1076c7
Abraham Tesfay <abraham@Abrahams-MacBook-Pro.local>
2023-05-23 22:29:35 -0700

    Add MyReservation page

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -43,7 +43,9 @@ const RegisterModal = () => {
     axios
       .post("/api/register", data)
       .then(() => {
+        toast.success('Registered');
         registerModal.onClose();
+        loginModal.onOpen();
       })
       .catch((error) => {
         toast.error(error);
