ce1f6cb26f663c6bec48fbd0a03329303977a668
Abraham Tesfay <abraham@Abrahams-MacBook-Pro.local>
2023-06-05 15:36:15 -0500

    Add MyProperties page

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,10 +1,38 @@
 import prisma from "@/app/libs/prismadb";
 
+export interface IListingsParams {
+  userId?: string;
+  // guestCount?: number;
+  // roomCount?: number;
+  // bathroomCount?: number;
+  // startDate?: string;
+  // endDate?: string;
+  // locationValue?: string;
+  // category?: string;
+}
 
-
-export default async function getListings() {
+export default async function getListings(
+  params: IListingsParams
+) {
 
   try {
+
+    const {
+      userId,
+      // roomCount, 
+      // guestCount, 
+      // bathroomCount, 
+      // locationValue,
+      // startDate,  
+      // endDate,
+      // category,
+    } = params;
+
+    let query: any = {};
+
+    if (userId) {
+      query.userId = userId;
+    }
     const listings = await prisma.listing.findMany({
 
       orderBy: {
